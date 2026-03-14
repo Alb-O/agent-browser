117713951658255644c9df31fac1c343016cbc45
Albert O'Shea <albertoshea2@gmail.com>
2026-03-14 19:50:57 +1100

    fix(nix): configure managed cargo source tree for package output
    - set managedCargo specPath from the repo root string
    - expose agent-browser as a buildRustPackage output
    - avoid duplicate poly-rust-env import in this repo

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,26 @@
 {
-  inputs,
+  config,
+  pkgs,
   ...
 }:
+
+let
+  standaloneProjectRoot = toString ./.;
+  agentBrowser = pkgs.rustPlatform.buildRustPackage {
+    pname = config.rustEnv.package.name;
+    version = config.rustEnv.package.version;
+    src = config.outputs.cargo_source_tree;
+    cargoLock.lockFile = ./Cargo.lock;
+    doCheck = false;
+  };
+in
 {
-  rustEnv.managedCargo.enable = true;
+  rustEnv.managedCargo = {
+    enable = true;
+    specPath = "${standaloneProjectRoot}/Cargo.poly.toml";
+  };
+
+  outputs.agent-browser = agentBrowser;
 
   scripts = {
     workspace-check.exec = ''
